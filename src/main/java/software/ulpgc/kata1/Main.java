a244436fb0085099f51e7186e662ea87c004803b
Enrique Fernández García <185592997+Enrickq@users.noreply.github.com>
2026-09-22 22:07:23 +0100

    Repetition 1: add application entry point

---
+++ src/main/java/software/ulpgc/kata1/Main.java
@@ -0,0 +1,20 @@
+package software.ulpgc.kata1;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person(
+                "Enrique",
+                LocalDate.of(2006, 7, 13)
+        );
+
+        LocalDate today = LocalDate.now();
+
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Birth date: " + person.getBirthDate());
+        System.out.println("Age: " + person.ageOn(today));
+    }
+}
