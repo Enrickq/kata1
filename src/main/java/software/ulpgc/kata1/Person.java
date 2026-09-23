4ff7b3127364bb648653998767d0c69527aa126b
Enrique Fernández García <185592997+Enrickq@users.noreply.github.com>
2026-09-23 09:03:59 +0100

    Repetition 2: implement Person domain model

---
+++ src/main/java/software/ulpgc/kata1/Person.java
@@ -0,0 +1,30 @@
+package software.ulpgc.kata1;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+
+    private final String name;
+    private final LocalDate birthDate;
+
+    public Person(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int ageOn(LocalDate date) {
+        if (date.isBefore(birthDate)) {
+            throw new IllegalArgumentException("Date cannot be before birth date");
+        }
+        return Period.between(birthDate, date).getYears();
+    }
+}
